049b24f1056eb575216aea52f38158c137cf7798
Dylan Young <dylanyoungmeijer@gmail.com>
2014-12-08 19:45:41 -0400

    Oops. Completed refactoring

--- application.js
+++ application.js
@@ -32,18 +32,6 @@ String.prototype.repeat = function (num) {
     });
   };
 
-  //Sortable Lists
-$(function () {
-    $(".list-sortable").sortable({
-        //tolerance: 'pointer',
-        containment: "parent",
-        //revert: 'invalid',
-        //placeholder: 'span2 well placeholder tile',
-        //forceHelperSize: true,
-        axis: "x"
-    });
-    $(".list-sortable").disableSelect();
-});
 
   $(function () {
 
